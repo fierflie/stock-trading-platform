596961da01b008738acac429330976b0727b7b97
fierflie <thakurpriyesh01@gmail.com>
2024-12-04 23:33:14 +0530

    Removed unnecessary files

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int a[]=new int[6];
-        a[5]=5;
-            System.out.println(a[5]);
-        a[6]=6;
-            System.out.println(a[4]);
-    }
-}
