4b62f2018e27f3332e41fe487247e2573b258782
fierflie <thakurpriyesh01@gmail.com>
2025-01-05 23:04:56 +0530

    Added missing files for stock trading platform project

---
+++ Main.java
@@ -0,0 +1,12 @@
+// Main.java
+// Entry point for the ProgressTrackerGUI project.
+
+import gui.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        // Launch the GUI application
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+}
